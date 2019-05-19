b4c976ec229256ccd09d165c18a4f508003414c7
SantiagoParedes97 <sparedes@est.frba.utn.edu.ar>
2019-05-19 15:51:07 -0300

    Arreglo los tests que habia roto en el merge.

--- app/src/test/java/ar/edu/utn/frba/mobile/a2019c1/mercury/model/ScheduleTest.kt
+++ app/src/test/java/ar/edu/utn/frba/mobile/a2019c1/mercury/model/ScheduleTest.kt
@@ -15,7 +15,7 @@ class ScheduleTest {
 
     @Test
     fun `Given a new Schedule, it has no clients`() {
-        assertThat(schedule.clients()).isEmpty()
+        assertThat(schedule.visits()).isEmpty()
     }
 
     @Test
@@ -25,56 +25,69 @@ class ScheduleTest {
 
     @Test
     fun `Given a new Schedule, can add a new Client on a specific day`() {
-        val aClient = Client("Tyrion Lannister", "1111111111", "King's Landing", LocalTime.of(10, 0))
+        val aClient = Client("Tyrion Lannister", "1111111111", "King's Landing")
+        val aVisit = Visit(aClient,LocalTime.of(10, 0))
+        schedule.addClientOnDay(1, aVisit)
 
-        schedule.addClientOnDay(1, aClient)
-
-        assertThat(schedule.clients()).hasSize(1)
-        assertThat(schedule.clients()).contains(aClient)
+        assertThat(schedule.visits()).hasSize(1)
+        assertThat(schedule.visits().contains(aVisit))
     }
 
     @Test
     fun `Given a new Schedule, can add multiple new Clients on a specific day`() {
-        val aClient = Client("Tyrion Lannister", "1111111111", "King's Landing", LocalTime.of(10, 0))
-        val anotherClient = Client("Jaime Lannister", "1111111111", "King's Landing", LocalTime.of(12, 0))
+        val aClient = Client("Tyrion Lannister", "1111111111", "King's Landing")
+        val anotherClient = Client("Jaime Lannister", "1111111111", "King's Landing")
 
-        schedule.addClientOnDay(1, aClient)
-        schedule.addClientOnDay(1, anotherClient)
+        val aVisit = Visit(aClient,LocalTime.of(10, 0))
+        val anotherVisit = Visit(anotherClient,LocalTime.of(12, 0))
 
-        assertThat(schedule.clients()).hasSize(2)
-        assertThat(schedule.clients()).containsExactlyInAnyOrder(aClient, anotherClient)
+
+        schedule.addClientOnDay(1, aVisit)
+        schedule.addClientOnDay(1, anotherVisit)
+
+        assertThat(schedule.visits()).hasSize(2)
+        assertThat(schedule.visits()).containsExactlyInAnyOrder(aVisit,anotherVisit)
     }
 
     @Test
     fun `Given a Schedule with multiple Clients on a specific day, its duration is of one day`() {
-        val aClient = Client("Tyrion Lannister", "1111111111", "King's Landing", LocalTime.of(10, 0))
-        val anotherClient = Client("Jaime Lannister", "1111111111", "King's Landing", LocalTime.of(12, 0))
+        val aClient = Client("Tyrion Lannister", "1111111111", "King's Landing")
+        val anotherClient = Client("Jaime Lannister", "1111111111", "King's Landing")
 
-        schedule.addClientOnDay(1, aClient)
-        schedule.addClientOnDay(1, anotherClient)
+        val aVisit = Visit(aClient,LocalTime.of(10, 0))
+        val anotherVisit = Visit(anotherClient,LocalTime.of(12, 0))
+
+        schedule.addClientOnDay(1, aVisit)
+        schedule.addClientOnDay(1, anotherVisit)
 
         assertThat(schedule.duration()).isEqualTo(1)
     }
 
     @Test
     fun `Given a new Schedule, can add multiple new Clients on a different days`() {
-        val aClient = Client("Tyrion Lannister", "1111111111", "King's Landing", LocalTime.of(10, 0))
-        val anotherClient = Client("Jaime Lannister", "1111111111", "King's Landing", LocalTime.of(12, 0))
+        val aClient = Client("Tyrion Lannister", "1111111111", "King's Landing")
+        val anotherClient = Client("Jaime Lannister", "1111111111", "King's Landing")
 
-        schedule.addClientOnDay(1, aClient)
-        schedule.addClientOnDay(3, anotherClient)
+        val aVisit = Visit(aClient,LocalTime.of(10, 0))
+        val anotherVisit = Visit(anotherClient,LocalTime.of(12, 0))
 
-        assertThat(schedule.clients()).hasSize(2)
-        assertThat(schedule.clients()).containsExactlyInAnyOrder(aClient, anotherClient)
+        schedule.addClientOnDay(1, aVisit)
+        schedule.addClientOnDay(3, anotherVisit)
+
+        assertThat(schedule.visits()).hasSize(2)
+        assertThat(schedule.visits()).containsExactlyInAnyOrder(aVisit, anotherVisit)
     }
 
     @Test
     fun `Given a Schedule with multiple Clients, its duration depends on the farthest day a client has to be visited`() {
-        val aClient = Client("Tyrion Lannister", "1111111111", "King's Landing", LocalTime.of(10, 0))
-        val anotherClient = Client("Jaime Lannister", "1111111111", "King's Landing", LocalTime.of(12, 0))
+        val aClient = Client("Tyrion Lannister", "1111111111", "King's Landing")
+        val anotherClient = Client("Jaime Lannister", "1111111111", "King's Landing")
 
-        schedule.addClientOnDay(1, aClient)
-        schedule.addClientOnDay(3, anotherClient)
+        val aVisit = Visit(aClient,LocalTime.of(10, 0))
+        val anotherVisit = Visit(anotherClient,LocalTime.of(12, 0))
+
+        schedule.addClientOnDay(1, aVisit)
+        schedule.addClientOnDay(3, anotherVisit)
 
         assertThat(schedule.duration()).isEqualTo(3)
     }
