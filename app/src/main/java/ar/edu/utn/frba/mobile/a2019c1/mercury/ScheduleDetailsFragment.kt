99cf9686bac2dba001ba50e74d8ca303b8a9d805
dperezferrando <diego@parsimotion.com>
2019-06-02 20:17:13 -0300

    Agrego onViewCreated al fragment

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/ScheduleDetailsFragment.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/ScheduleDetailsFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import ar.edu.utn.frba.mobile.a2019c1.mercury.model.Schedule
+import kotlin.reflect.KProperty
 
 class ScheduleDetailsFragment : Fragment() {
+
+    private val scheduleDetailsViewModel: ScheduleDetailsViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +21,19 @@ class ScheduleDetailsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_schedule_details, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        scheduleDetailsViewModel.scheduleToView?.let {
+            loadScheduleToViewFromViewModel(it)
+            scheduleDetailsViewModel.scheduleToView = null
+        }
+    }
+
+    private fun loadScheduleToViewFromViewModel(it: Schedule) {
+
+    }
+
 }
+
+
