cdb71d1da6a9cc372cf1986099cdaf947aa9f50d
SantiagoParedes97 <sparedes@est.frba.utn.edu.ar>
2019-06-03 22:20:02 -0300

    Refactor de la clase WhatsappIntegration. Ahora tambien verifica que la app este instalada.

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/util/WhatsappIntegration.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/util/WhatsappIntegration.kt
@@ -1,7 +1,10 @@
 package ar.edu.utn.frba.mobile.a2019c1.mercury.util
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AlertDialog
 
 
 object WhatsappIntegration {
@@ -11,14 +14,50 @@ object WhatsappIntegration {
         phoneNumber: String?,
         activity: Activity
     ) {
+        if(isAppAvailable(activity,"com.whatsapp")){
+            launchWhatsappIntent(phoneNumber, textMessage, activity)
+        }
+        else{
+            showAlertDialog(activity)
+        }
+
+    }
+
+    private fun launchWhatsappIntent(
+        phoneNumber: String?,
+        textMessage: String?,
+        activity: Activity
+    ) {
         val number = formatPhoneNumber(phoneNumber)
         val sendIntent = Intent();
         sendIntent.action = Intent.ACTION_SEND
-        sendIntent.putExtra(Intent.EXTRA_TEXT,textMessage)
+        sendIntent.putExtra(Intent.EXTRA_TEXT, textMessage)
         sendIntent.type = "text/plain"
         sendIntent.setPackage("com.whatsapp")
         sendIntent.putExtra("jid", "$number@s.whatsapp.net");
         activity.startActivity(sendIntent)
+    }
+
+    private fun showAlertDialog(activity: Context) {
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle("No se puede enviar mensajes a whatsapp.")
+        builder.setCancelable(true)
+        builder.setMessage("No tiene instalado Whatsapp")
+        builder.setPositiveButton("Aceptar") { _, _ ->  }
+        builder.show()
+
+    }
+
+
+    fun isAppAvailable(context: Context, appName: String): Boolean {
+        val pm = context.packageManager
+        return try {
+            pm.getPackageInfo(appName, PackageManager.GET_ACTIVITIES)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+
     }
 
     private fun formatPhoneNumber(phoneNumber: String?) = phoneNumber?.replace("+", "")?.replace(" ", "")
