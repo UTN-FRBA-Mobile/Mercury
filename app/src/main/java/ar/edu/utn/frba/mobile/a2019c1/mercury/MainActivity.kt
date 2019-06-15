ad97baf7a0921fb3c46b126755b9dbc189f8f46b
Nico <nmrainhart@gmail.com>
2019-06-15 14:37:04 -0300

    Remove Settings button from Action Bar

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/MainActivity.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/MainActivity.kt
@@ -7,8 +7,6 @@ import android.content.pm.PackageManager
 import android.database.Cursor
 import android.os.Bundle
 import android.provider.ContactsContract
-import android.view.Menu
-import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -35,26 +33,7 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.fragmentContainer, ScheduleListFragment())
                 .commit()
         }
-
-
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
 
     override fun onAttachFragment(fragment: Fragment) {
         if (fragment is ScheduleEditionFragment) {
