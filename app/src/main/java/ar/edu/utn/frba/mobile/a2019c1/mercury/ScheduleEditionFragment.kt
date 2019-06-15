673d36a93972a01310a44752d08ec6d46d071e4c
Nico <nmrainhart@gmail.com>
2019-06-15 11:55:39 -0300

    Validate client data is not blank when adding it to schedule

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/ScheduleEditionFragment.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/ScheduleEditionFragment.kt
@@ -8,6 +8,7 @@ import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -30,6 +31,7 @@ class ScheduleEditionFragment : Fragment(), ScheduleEditionAdapter.OnItemClickLi
     private val clientsPerDay: MutableList<Pair<Int,Visit>> = mutableListOf()
     private lateinit var onEditionCompleted: () -> Unit
 
+    private fun newClientInputFields() = listOf(client_name, client_phone_number, client_location)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -91,17 +93,17 @@ class ScheduleEditionFragment : Fragment(), ScheduleEditionAdapter.OnItemClickLi
     }
 
     private fun addClientToSchedule() {
-        val name = client_name.text.toString()
-        val phoneNumber = client_phone_number.text.toString()
-        val location = client_location.text.toString()
+        val isClientDataValid = validateClientDataIsNotEmpty()
+        if(!isClientDataValid) return
+
+        val clientToAdd = parseNewClient()
         val visitTime: LocalTime =
-        try {
-            LocalTime.parse(client_visit_time.text.toString())
-        } catch (e: DateTimeParseException) {
-            client_visit_time.error = "Formato Invalido de fecha"
-            return
-        }
-        val clientToAdd = Client(name, phoneNumber, location)
+            try {
+                LocalTime.parse(client_visit_time.text.toString())
+            } catch (e: DateTimeParseException) {
+                client_visit_time.error = "Formato Invalido de fecha"
+                return
+            }
         val dayNumber = 1 // TODO get actual day
         val visit = Visit(clientToAdd,visitTime)
         clientsPerDay.add(Pair(dayNumber,visit))
@@ -110,8 +112,22 @@ class ScheduleEditionFragment : Fragment(), ScheduleEditionAdapter.OnItemClickLi
         updateAdapter()
     }
 
+    private fun validateClientDataIsNotEmpty(): Boolean {
+        val blankInputFields: List<EditText> = newClientInputFields().filter { it.text.isBlank() }
+        blankInputFields.forEach { it.error = "No puede ser vacío" }
+        return blankInputFields.isEmpty()
+    }
+
+    private fun parseNewClient(): Client {
+        val name = client_name.text.toString()
+        val phoneNumber = client_phone_number.text.toString()
+        val location = client_location.text.toString()
+        return Client(name, phoneNumber, location)
+    }
+
     private fun clearNewClientFields() {
-        listOf(client_name, client_phone_number, client_location).forEach {
+        val newClientInputFields = newClientInputFields()
+        newClientInputFields.forEach {
             it.text.clear()
         }
         client_visit_time.text = "00:00"
