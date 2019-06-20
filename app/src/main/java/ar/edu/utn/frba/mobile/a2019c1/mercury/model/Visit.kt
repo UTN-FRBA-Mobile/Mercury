3ec4256f79a28aa38fafbc1148e66e2937f3186e
Nico <nmrainhart@gmail.com>
2019-06-20 16:39:12 -0300

    Start Schedules on a given day

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/model/Visit.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/model/Visit.kt
@@ -1,5 +1,10 @@
 package ar.edu.utn.frba.mobile.a2019c1.mercury.model
 
+import java.time.LocalDate
 import java.time.LocalTime
 
-data class Visit(val client : Client, val timeToVisit: LocalTime)
+data class Visit(val client : Client, val timeToVisit: LocalTime, val visitDates: MutableList<LocalDate> = mutableListOf()) {
+    fun addVisitOnDate(date: LocalDate) {
+        visitDates.add(date)
+    }
+}
