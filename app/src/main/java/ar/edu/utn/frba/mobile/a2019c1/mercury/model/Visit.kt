1eb8e0827820778295474c2d6e4dfe8d9d28fa08
Nico <nmrainhart@gmail.com>
2019-06-22 16:55:23 -0300

    Add Visit#nextVisitDate

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/model/Visit.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/model/Visit.kt
@@ -7,4 +7,10 @@ data class Visit(val client : Client, val timeToVisit: LocalTime, val visitDates
     fun addVisitOnDate(date: LocalDate) {
         visitDates.add(date)
     }
+
+    fun nextVisitDate(date: LocalDate): LocalDate? {
+        // There should be no more than one possible next visit date. If there are, the first one is picked
+        return visitDates.sortedBy { it }
+            .firstOrNull { it.isEqual(date) || it.isAfter(date) }
+    }
 }
