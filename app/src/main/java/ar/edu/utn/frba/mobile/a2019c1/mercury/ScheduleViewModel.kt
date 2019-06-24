b983448bb00546dd2fce3b23e14308c1a8dbcf48
lclauser <lclauser@est.frba.utn.edu.ar>
2019-06-24 04:38:29 -0300

    Upsert y delete por db

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/ScheduleViewModel.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/ScheduleViewModel.kt
@@ -1,32 +1,18 @@
 package ar.edu.utn.frba.mobile.a2019c1.mercury
 
 import androidx.lifecycle.ViewModel
+import ar.edu.utn.frba.mobile.a2019c1.mercury.db.Database
 import ar.edu.utn.frba.mobile.a2019c1.mercury.model.Schedule
 
 class ScheduleViewModel : ViewModel() {
     val schedules: MutableList<Schedule> = mutableListOf()
 
     fun remove(scheduleToDelete: Schedule) {
-        schedules.remove(scheduleToDelete)
+        Database.delete(scheduleToDelete)
     }
 
-    fun upsert(scheduleToUpsert: Schedule): List<Schedule> {
-        val scheduleExists = schedules.any { it.name == scheduleToUpsert.name }
-        if(scheduleExists) {
-            updateSchedule(scheduleToUpsert)
-        } else {
-            schedules.add(scheduleToUpsert)
-        }
-        return schedules
+    fun upsert(scheduleToUpsert: Schedule) {
+        Database.save(scheduleToUpsert)
     }
 
-    private fun updateSchedule(scheduleToUpsert: Schedule) {
-        schedules.replaceAll { existingSchedule ->
-            if (existingSchedule.name == scheduleToUpsert.name) {
-                scheduleToUpsert
-            } else {
-                existingSchedule
-            }
-        }
-    }
 }
