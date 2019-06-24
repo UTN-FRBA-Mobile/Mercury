df556877b5317c2f12b53622c831477e05d4e9c5
lclauser <lclauser@est.frba.utn.edu.ar>
2019-06-24 04:40:30 -0300

    agrego listener de firebase para suscribirse a los cambios

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/ScheduleListFragment.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/mercury/ScheduleListFragment.kt
@@ -8,7 +8,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import ar.edu.utn.frba.mobile.a2019c1.mercury.db.Database
 import ar.edu.utn.frba.mobile.a2019c1.mercury.model.Schedule
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.fragment_schedule_edition.fab
 import kotlinx.android.synthetic.main.fragment_schedule_list.*
 
@@ -18,6 +22,12 @@ class ScheduleListFragment : Fragment() {
     private lateinit var onAddScheduleButtonClicked: () -> Unit
     private lateinit var onViewScheduleButtonClicked: (Schedule) -> Unit
     private lateinit var onEditScheduleButtonClicked: (Schedule) -> Unit
+    lateinit var scheduleListAdapter : ScheduleListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Database.db.orderByKey().addValueEventListener(scheduleListener)
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,7 +47,7 @@ class ScheduleListFragment : Fragment() {
 
     private fun setupAdapter() {
         val schedules = viewModel.schedules
-        val scheduleListAdapter = ScheduleListAdapter(
+        scheduleListAdapter = ScheduleListAdapter(
             context!!,
             schedules,
             this.onViewScheduleButtonClicked,
@@ -71,5 +81,34 @@ class ScheduleListFragment : Fragment() {
     fun setOnEditScheduleButtonClicked(onEditScheduleButtonClicked: (Schedule) -> Unit) {
         this.onEditScheduleButtonClicked = onEditScheduleButtonClicked
     }
+
+    private fun loadTaskList(dataSnapshot: DataSnapshot) {
+
+        val schedules = dataSnapshot.children.iterator()
+        if (schedules.hasNext()) {
+            viewModel.schedules.clear()
+            val listIndex = schedules.next()
+            val itemsIterator = listIndex.children.iterator()
+            while (itemsIterator.hasNext()) {
+                val currentItem = itemsIterator.next()
+                val map = currentItem.getValue() as HashMap<String, Any>
+                val schedule = Schedule.buildFromDatabase(map)
+                viewModel.schedules!!.add(schedule)
+            }
+        }
+        scheduleListAdapter.notifyDataSetChanged()
+        if (viewModel.schedules.isNotEmpty()) {
+            schedule_list.visibility = View.VISIBLE
+            empty_view.visibility = View.GONE
+        }
+    }
+
+    var scheduleListener: ValueEventListener = object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            loadTaskList(dataSnapshot)
+        }
+        override fun onCancelled(databaseError: DatabaseError) {}
+    }
+
 
 }
